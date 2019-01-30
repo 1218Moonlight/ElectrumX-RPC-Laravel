f4ef350712ce7a96801121b53106dacf6548e1c3
kmoon <1218moonlight@gmail.com>
2019-01-30 14:12:20 +0900

    getinfo API 작업. getinfo API 作業。

--- contents/html/laravel/resources/views/rpc/getinfo.blade.php
+++ contents/html/laravel/resources/views/rpc/getinfo.blade.php
@@ -1,2 +1,28 @@
 <?php
-    echo "hello";
+
+class getinfo
+{
+    private $socket;
+
+    function __construct($ip, $port)
+    {
+        $this->socket = fsockopen($ip, $port, $errno, $errstr, 3);
+    }
+
+    function send()
+    {
+        fwrite($this->socket, '{"jsonrpc": "2.0", "id": null, "method": "getinfo", "params": []}');
+        fwrite($this->socket, "\n");
+        fflush($this->socket);
+        $response = fgets($this->socket);
+        echo $response;
+    }
+}
+
+function main()
+{
+    $getinfo = new getinfo("ip", "port");
+    $getinfo->send();
+}
+
+main();
